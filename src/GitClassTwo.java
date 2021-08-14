d65e6336b45b816c8cc4397cbbeea77b4fea1992
Sameer <sameer.shivane3362@gmail.com>
2021-08-15 00:34:01 +0530

    again commited by INdian guy

--- src/GitClassTwo.java
+++ src/GitClassTwo.java
@@ -9,6 +9,11 @@ public class GitClassTwo {
 		System.out.println("2.3");
 		System.out.println("2.4");
 		System.out.println("2.5");
+		System.out.println("2.6");
+		System.out.println("2.7");
+		System.out.println("2.8");
+		System.out.println("2.9");
+		System.out.println("2.10");
 	}
 
 }
