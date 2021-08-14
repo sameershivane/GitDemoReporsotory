a95c4f6e56235177bf87c9e737896bc7ff91146b
Sameer <sameer.shivane3362@gmail.com>
2021-08-14 23:39:55 +0530

    Commited from india

---
+++ src/GitClassTwo.java
@@ -0,0 +1,14 @@
+
+public class GitClassTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("2.1");
+		System.out.println("2.2");
+		System.out.println("2.3");
+		System.out.println("2.4");
+		System.out.println("2.5");
+	}
+
+}
---
+++ src/GitClassone.java
@@ -0,0 +1,14 @@
+
+public class GitClassone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("1.1");
+		System.out.println("1.2");
+		System.out.println("1.3");
+		System.out.println("1.4");
+		System.out.println("1.5");
+	}
+
+}
